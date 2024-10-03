03e5e5edd503032988b74e7b97fd2929a8dcdc99
Suriyaprakash1101 <123788954+Suriyaprakash1101@users.noreply.github.com>
2024-10-03 21:05:42 +0530

    Create pattern3.java

---
+++ pattern3.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the Character");
+// 		int n=(int)in.next().charAt(0)-65;
+        int n=5;
+		int count=0;
+		for(int i=n;i>0;i--){
+		    for(int j=i;j>0;j--){
+		        System.out.print(i);
+		    }
+		    System.out.println();
+		}
+
+	}
+}
+/*
+55555
+4444
+333
+22
+1
+*/
