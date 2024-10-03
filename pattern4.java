c0b3d7559cbfbeccef91d375952110345e29f9da
Suriyaprakash1101 <123788954+Suriyaprakash1101@users.noreply.github.com>
2024-10-03 21:08:21 +0530

    Create pattern4.java

---
+++ pattern4.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		
+        int n=5;
+		
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=i;j++){
+		        System.out.print(j);
+		    }
+		    System.out.println();
+		}
+
+	}
+}
+/*
+1
+12
+123
+1234
+12345
+
+*/
